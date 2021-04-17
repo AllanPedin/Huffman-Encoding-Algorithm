1abf2f696069e1c873891bc7c9a07e9cd9163926
Allan Pedin <allan.pedin.17@cnu.edu>
2021-04-16 21:31:22 -0400

    better printing

--- tests.py
+++ tests.py
@@ -2,18 +2,18 @@ import huffman
 import node
 
 def testHuffman(r, probability_distribution):
-    print_new_test_case(r, probability_distribution)
     encoder = huffman.Huffman_Encoder(r)
     encoder.buildTree(probability_distribution)
-    # node.print_tree(encoder.tree_base, 0) #good for debugging
+    print("Generated Tree:")
+    node.print_tree(encoder.tree_base, 0) 
     encoder.encode()
+    print("Encoding:")
     print(encoder.encoding)
 
-def print_new_test_case(r, probability_distribution):
-    print("-------TEST CASE-------")
+def print_new_test_case(test_num , r, probability_distribution):
+    print("-------TEST CASE "+str(test_num) +"-------")
     print("r = ", r)
-    print("pdist = ", probability_distribution)
-    print("Encoding:\n")
+    print("pdist = ", probability_distribution, "Length = ", len(probability_distribution))
 tests = [
     #(r, probability_distribution)
     (2,[0.44,0.21,0.12,0.1,0.07]),
@@ -25,5 +25,7 @@ tests = [
     (4,[0.1859,0.1031,0.0796,0.0642,0.0632,0.0575,0.0574,0.0514,0.0484,0.0467,0.0321,0.0317,0.0228,0.0218,0.0208,0.0198,0.0175,0.0164,0.0152,0.0152,0.0127,0.0083,0.0049,0.0013,0.0008,0.0008,0.0005]),
     (5,[0.1859,0.1031,0.0796,0.0642,0.0632,0.0575,0.0574,0.0514,0.0484,0.0467,0.0321,0.0317,0.0228,0.0218,0.0208,0.0198,0.0175,0.0164,0.0152,0.0152,0.0127,0.0083,0.0049,0.0013,0.0008,0.0008,0.0005])
 ] 
-for r, probability_distribution in tests:
+for i in range(0, len(tests)):
+    r, probability_distribution = tests[i]
+    print_new_test_case(i, r, probability_distribution)
     testHuffman(r, probability_distribution)
