8c3a4361f6a5bc4122befc23ce110d460a2d0f02
Allan Pedin <allan.pedin.17@cnu.edu>
2021-04-16 20:14:49 -0400

    tree constructed properly and tests implemented sort of

--- huffman.py
+++ huffman.py
@@ -5,28 +5,39 @@ import node
 class Huffman_Encoder:
     def __init__(self, r):
         self.r = r
-    def encode(self, probability_distribution):
-        nodes = list(map(self.probabilty_to_node, probability_distribution))
+        self.tree_base = None
+    def buildTree(self, probability_distribution):
+        nodes = self.convert_to_nodes(probability_distribution)
         while len(nodes)>1:
             nodes.sort(key = self.take_node_value)
             group = nodes[:self.r] #get first r from our list
-            new_node = node.node(self.r, group, self.sum_node_probabilities(group)) #new node which contains first r nodes
+            new_node = node.node(self.r, group, self.sum_node_probabilities(group), "P") #new node which contains first r nodes
             nodes = nodes[self.r:] #remove first r items
             nodes.append(new_node) #replace first r items with node containing them as edges
-        print_tree(nodes[0])
+        self.tree_base = nodes[0]
+        # print(self.encode_tree(nodes[0], ""))
+    #base_node : base of your tree
+    #path : path so to base_node so far (empty string at stump)
+    #returns: encoding for tree
+    def encode_tree(self, base_node, path):
+        if base_node.is_leaf():
+            return [path]
+        else:
+            encoding = []
+            edges = base_node.edges
+            for i in range(len(edges)):
+                subtree = self.encode_tree(edges[i], path + str(i))
+                encoding.extend(subtree)
+            return encoding
     def sum_node_probabilities(self, nodes):
         sum = 0
         for node in nodes:
-            sum = sum + node.value
+            sum = sum + node.probability
         return sum
-    def probabilty_to_node(self, probability):
-        return node.node(self.r, [], probability)
+    def convert_to_nodes(self, probability_distribution):
+        nodes = []
+        for i in range(0,len(probability_distribution)):
+            nodes.append(node.node(self.r, [], probability_distribution[i],"S"+str(i+1)))
+        return nodes
     def take_node_value(self, node):
-        return node.value
-def print_tree(base_node):
-    print(base_node.value)
-    for node in base_node.edges:
-        print_tree(node)
-
-encoder = Huffman_Encoder(2)
-encoder.encode([.2,.3,.4,.1])
+        return node.probability
--- node.py
+++ node.py
@@ -1,9 +1,30 @@
 class node:
-    #r : r-ary node (has r edges)
+    #r: r-ary node (has r edges)
     #edges: list of other nodes max length should be r
-    def __init__(self, r, edges, value):
+    #probability: probability of this symbol occuring
+    #symbol: symbol
+    def __init__(self, r, edges, probability, symbol):
         if r < len(edges):
             raise Exception("More edges than r, r=" + r + " edges = ", edges)
         self.r = r
         self.edges = edges
-        self.value = value
+        self.probability = probability
+        self.symbol =  symbol
+    def to_string(self):
+        return self.symbol + "("+str(self.probability) +")"
+    def is_leaf(self):
+        if len(self.edges) <=0:
+            return True
+        else:
+            return False
+def print_tree(base_node, depth):
+    tabs = get_tabs(depth)
+    print(tabs, base_node.to_string() )
+
+    for sub_node in base_node.edges:
+        print_tree(sub_node, depth  + 1)
+def get_tabs(num):
+    tabs = ""
+    for i in range(0, num):
+        tabs = tabs + "\t"
+    return tabs
